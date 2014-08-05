acb3bed2eef8b5446aa7a8dd3201aee898652267
Nikolay <Werevolff>
2014-08-05 16:35:09 +1100

    First class

--- molokai_cms/__init__.py
+++ molokai_cms/__init__.py
@@ -2,12 +2,34 @@
 Molokai CMS
 """
 from flask import Flask
-import os
 
-BASE_DIR = os.path.dirname(__file__)
 
-molokai = Flask('molokai_cms')
+class MolokaiCMS(object):
+    """
+    Base class to initialize Molokai CMS
 
-@molokai.route('/')
-def hello_world():
-    return molokai.auto_find_instance_path()
+    Arguments:
+        project_dir (str): Full path to main application
+        project_name (str): Name of application
+
+    Example:
+    in /path/to/main/application/app.py
+
+        from molokai_cms import MolokaiCMS
+
+        molokai = MolokaiCMS('/path/to/main/application/', 'My Internet Page')
+
+        if __name__ == '__main__':
+            molokai.app.run()
+
+    Then in terminal:
+
+        $ python /path/to/main/application/app.py
+
+             * Running on http://127.0.0.1:5000/
+    """
+
+    def __init__(self, project_dir, project_name):
+        self.project_dir = project_dir
+        self.project_name = project_name
+        self.app = Flask(self.project_name)
